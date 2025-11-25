755a11c203bc4db6442c41be32b0efe761de86c7
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-11-25 23:56:31 +0300

    update on app .py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 # A new look at this project
+
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+
+    
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+    @app.route('/hello')
+    @app.route('/hello/<name>')
+    def hello(name=None):
+        return render_template('hello.html', name=name)
+    return app
