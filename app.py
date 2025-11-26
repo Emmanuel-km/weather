21d0a7c02e26f595a8d9ef0feec96a90c00d6b57
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-11-26 21:01:48 +0300

    running

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 # A new look at this project
+from flask import Flask, send_file
 
-from flask import Flask, render_template
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-
-
-    
-    @app.route('/')
-    def home():
-        return render_template('index.html')
-    @app.route('/hello')
-    @app.route('/hello/<name>')
-    def hello(name=None):
-        return render_template('hello.html', name=name)
-    return app
+@app.route('/')
+def home():
+    return send_file('index.html')
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
