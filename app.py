4ac07d0b4403e657c95198ac594201bf746f309e
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-12-02 23:57:17 +0300

    now looking good

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-# A new look at this project
-from flask import Flask, send_file
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return send_file('index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/welcome', methods=['POST'])
+def welcome():
+    name = request.form.get('name')
+    return render_template('welcome.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
