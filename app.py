9a1981eac028880ce4bb63adc5a6fd54a5e66c6b
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-12-19 23:09:42 +0300

    responce handling

--- app.py
+++ app.py
@@ -9,6 +9,26 @@ url=f"{base_url}?access_key={key}&query={city}"
 
 responce=requests.get(url)
 data=responce.json()
-#data=pd.DataFrame(data)
 
-print(data)
+try:
+    weather_data = {
+        "Country": [data['location']['country']],
+        "City": [data['location']['name']],
+        "Region": [data['location']['region']],
+        "Temperature": [data['current']['temperature']],
+        "sunrise": [data['astro']['sunrise']] if 'astro' in data else ['N/A'],
+        "sunset": [data['astro']['sunset']] if 'astro' in data else ['N/A'],
+        "Moon phase": [data['astro']['moon_phase']] if 'astro' in data else ['N/A'],
+        "Wind speed": [data['current']['wind_speed']],
+        "wind degree": [data['current']['wind_degree']],
+        "wind direction": [data['current']['wind_dir']],
+        "cloudcover": [data['current']['cloudcover']]
+    }
+
+    df = pd.DataFrame(weather_data)
+    print(df)
+except KeyError:
+    print("Error retrieving data:", data.get("error", "Unknown error"))
+
+except Exception as e:
+    print("An unexpected error occurred:", str(e))
